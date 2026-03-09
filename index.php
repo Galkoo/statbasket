16f3a8083a4816dfb1a6b84627d00b74892128cd
Galkoo <154027102+Galkoo@users.noreply.github.com>
2026-03-09 17:59:25 +0100

    Refactor index.php for improved structure and styling

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
 <?php
 session_start();
-require_once __DIR__ . '/povezava.php';
+require_once "povezava.php";
 ?>
 
 <!DOCTYPE html>
-<html lang="sl">
+<html>
 <head>
 <meta charset="UTF-8">
 <title>StatBasket</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
 <div class="container">
 
-<h1>StatBasket</h1>
+<h1>🏀 StatBasket</h1>
+<p class="subtitle">Statistika košarkarskih tekem in igralcev</p>
 
-<p class="podnaslov">
-Statistika košarkarskih tekem in igralcev
-</p>
+<div class="menu">
 
-<?php if(isset($_SESSION["prijavljen"])): ?>
-
-<p>
-Prijavljen kot:
-<strong><?= htmlspecialchars($_SESSION["uporabnik"]["Ime"]) ?></strong>
-(<?= htmlspecialchars($_SESSION["uporabnik"]["Vloga"]) ?>)
-</p>
-
-<a href="logout.php">Odjava</a>
-
-<?php else: ?>
+<?php if(!isset($_SESSION["prijavljen"])): ?>
 
 <a href="prijava.php">Prijava</a>
 <a href="registracija.php">Registracija</a>
 
+<?php else: ?>
+
+<a href="dodaj_ekipo.php">Dodaj ekipo</a>
+<a href="dodaj_igralca.php">Dodaj igralca</a>
+<a href="dodaj_tekmo.php">Dodaj tekmo</a>
+<a href="vnos_statistike.php">Dodaj statistiko</a>
+<a href="urejanje_statistik.php">Urejanje statistik</a>
+<a href="logout.php">Odjava</a>
+
 <?php endif; ?>
+
+</div>
 
 <hr>
 
@@ -44,36 +44,22 @@ Prijavljen kot:
 
 <?php
 
-$query = "
-SELECT
-t.Datum,
-ed.Ime_ekipe AS Domaca,
-eg.Ime_ekipe AS Gostujoca,
-t.Rezultat_Dom,
-t.Rezultat_Gos
+$sql = "SELECT t.Datum, ed.Ime_ekipe AS Domaca, eg.Ime_ekipe AS Gostujoca,
+t.Rezultat_Dom, t.Rezultat_Gos
 FROM Tekme t
 INNER JOIN Ekipe ed ON t.Ekipa_Dom = ed.e_id
 INNER JOIN Ekipe eg ON t.Ekipa_Gos = eg.e_id
-ORDER BY t.Datum DESC
-";
+ORDER BY t.Datum DESC";
 
-$rezultat = mysqli_query($conn,$query);
+$result = mysqli_query($conn,$sql);
 
-if(mysqli_num_rows($rezultat) > 0){
+if(mysqli_num_rows($result)>0){
 
 echo "<ul>";
 
-while($t = mysqli_fetch_assoc($rezultat)){
+while($row=mysqli_fetch_assoc($result)){
 
-echo "<li>";
-
-echo htmlspecialchars($t['Datum'])." | ";
-echo htmlspecialchars($t['Domaca'])." ";
-echo htmlspecialchars($t['Rezultat_Dom'])." : ";
-echo htmlspecialchars($t['Rezultat_Gos'])." ";
-echo htmlspecialchars($t['Gostujoca']);
-
-echo "</li>";
+echo "<li>{$row['Datum']} | {$row['Domaca']} {$row['Rezultat_Dom']} : {$row['Rezultat_Gos']} {$row['Gostujoca']}</li>";
 
 }
 
@@ -81,7 +67,7 @@ echo "</ul>";
 
 }else{
 
-echo "Ni vnešenih tekem.";
+echo "Ni tekem.";
 
 }
 
