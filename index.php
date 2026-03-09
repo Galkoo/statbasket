b9cd3e726385d8000e6c71bc3002e2a534804dea
Galkoo <154027102+Galkoo@users.noreply.github.com>
2026-03-09 18:41:59 +0100

    Refactor index.php for session handling and display

--- index.php
+++ index.php
@@ -16,7 +16,14 @@ require_once "povezava.php";
 <div class="container">
 
 <h1>🏀 StatBasket</h1>
-<p class="subtitle">Statistika košarkarskih tekem in igralcev</p>
+
+<?php if(isset($_SESSION["prijavljen"])): ?>
+
+<p>Prijavljen kot:
+<?= $_SESSION["uporabnik"]["Ime"] ?>
+(<?= $_SESSION["uporabnik"]["Vloga"] ?>)</p>
+
+<?php endif; ?>
 
 <div class="menu">
 
@@ -27,11 +34,22 @@ require_once "povezava.php";
 
 <?php else: ?>
 
+<?php if($_SESSION["uporabnik"]["Vloga"]=="admin" || $_SESSION["uporabnik"]["Vloga"]=="sodnik"): ?>
+
 <a href="dodaj_ekipo.php">Dodaj ekipo</a>
 <a href="dodaj_igralca.php">Dodaj igralca</a>
 <a href="dodaj_tekmo.php">Dodaj tekmo</a>
 <a href="vnos_statistike.php">Dodaj statistiko</a>
 <a href="urejanje_statistik.php">Urejanje statistik</a>
+
+<?php endif; ?>
+
+<?php if($_SESSION["uporabnik"]["Vloga"]=="admin"): ?>
+
+<a href="admin.php">Admin panel</a>
+
+<?php endif; ?>
+
 <a href="logout.php">Odjava</a>
 
 <?php endif; ?>
@@ -40,41 +58,30 @@ require_once "povezava.php";
 
 <hr>
 
-<h2>Zadnje tekme</h2>
+<h2>Tekme</h2>
 
 <?php
 
-$sql = "SELECT t.Datum, ed.Ime_ekipe AS Domaca, eg.Ime_ekipe AS Gostujoca,
-t.Rezultat_Dom, t.Rezultat_Gos
+$query="SELECT t.Datum,ed.Ime_ekipe AS Domaca,eg.Ime_ekipe AS Gostujoca,
+t.Rezultat_Dom,t.Rezultat_Gos
 FROM Tekme t
-INNER JOIN Ekipe ed ON t.Ekipa_Dom = ed.e_id
-INNER JOIN Ekipe eg ON t.Ekipa_Gos = eg.e_id
+INNER JOIN Ekipe ed ON t.Ekipa_Dom=ed.e_id
+INNER JOIN Ekipe eg ON t.Ekipa_Gos=eg.e_id
 ORDER BY t.Datum DESC";
 
-$result = mysqli_query($conn,$sql);
+$rez=mysqli_query($conn,$query);
 
-if(mysqli_num_rows($result)>0){
+while($row=mysqli_fetch_assoc($rez)){
 
-echo "<ul>";
-
-while($row=mysqli_fetch_assoc($result)){
-
-echo "<li>{$row['Datum']} | {$row['Domaca']} {$row['Rezultat_Dom']} : {$row['Rezultat_Gos']} {$row['Gostujoca']}</li>";
-
-}
-
-echo "</ul>";
-
-}else{
-
-echo "Ni tekem.";
+echo $row["Datum"]." | ".$row["Domaca"]." ".
+$row["Rezultat_Dom"]." : ".
+$row["Rezultat_Gos"]." ".
+$row["Gostujoca"]."<br>";
 
 }
 
 ?>
 
 </div>
-
 </body>
 </html>
-
