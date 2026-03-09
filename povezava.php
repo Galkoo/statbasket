af77609990c3779dc983e23159338a49ea8ef445
Galkoo <154027102+Galkoo@users.noreply.github.com>
2026-03-09 17:59:00 +0100

    Change database username and password
    Updated database connection credentials.

--- povezava.php
+++ povezava.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "statbasket";
+$password = "StatBasket123!";
 $dbname = "statbasket";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
