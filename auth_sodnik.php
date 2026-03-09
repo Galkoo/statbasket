7cc6781809b499d186b0f5ff3f1930259cce19d9
Galkoo <154027102+Galkoo@users.noreply.github.com>
2026-03-09 18:41:35 +0100

    Add session validation for user roles in auth_sodnik.php

--- auth_sodnik.php
+++ auth_sodnik.php
@@ -1,1 +1,16 @@
 
+<?php
+session_start();
+
+if(!isset($_SESSION["prijavljen"])){
+    header("Location: prijava.php");
+    exit();
+}
+
+$vloga=$_SESSION["uporabnik"]["Vloga"];
+
+if($vloga!="sodnik" && $vloga!="admin"){
+    header("Location: index.php");
+    exit();
+}
+?>
